307f10fb4981aa1f41572fc6c1d40c52b2361b8b
Herman J. Radtke III <hermanradtke@gmail.com>
2013-01-05 04:56:01 -0800

    Started getting login working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Happy::Application.routes.draw do
-  resources :thoughts do
-      get 'random', :on => :collection
-  end
+    get 'signup' => 'users#new', :as => 'signup'
+    get 'login' => 'sessions#new', :as => 'login'
+    get 'logout' => 'sessions#destroy', :as => 'logout'
 
-  resources :cheer_up, :path => '/cheer-up'
+    resources :users
+    resources :sessions
 
-  root :to => 'thoughts#index'
+    resources :thoughts do
+        get 'random', :on => :collection
+    end
 
-  # See how all your routes lay out with "rake routes"
+    resources :cheer_up, :path => '/cheer-up'
+
+    root :to => 'thoughts#index'
+
+    # See how all your routes lay out with "rake routes"
 end
